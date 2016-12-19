c60350a2833da93be34934e4c4e44418f5e0a5c9
Glynn Bird <glynn.bird@gmail.com>
2016-12-19 11:51:46 +0000

    electron deployment tracker

--- tracker.js
+++ tracker.js
@@ -2,78 +2,76 @@
 
 'use strict';
 
-var restler = require('restler'),
-    path = require('path'),
-    cwd = require('cwd');
+var path = require('path'),
+    https = require('https'),
+    cwd = process.cwd();
+
+var HTTPPost = function(body, callback) {
+
+    // An object of options to indicate where to post to
+    var b = JSON.stringify(body);
+    console.log(b);
+    var post_options = {
+      host: 'deployment-tracker.mybluemix.net',
+      port: 443,
+      path: '/api/v1/track',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(b)
+      }
+    };
+
+    // Set up the request
+    var post_req = https.request(post_options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function(chunk) {
+        //console.log('Response: ', res.statusCode, chunk);
+      });
+      res.on('end', callback);
+    });
+
+    // post the data
+    post_req.write(new Buffer(b, 'utf8'));
+    post_req.end();
+};
 
 function track() {
     var pkg = null;
     try {
-        pkg = require(path.join(cwd(), 'package.json'));
+      pkg = require(path.join(cwd, 'package.json'));
     }
-    catch(ex) {
-        // package.json could not be loaded from the cwd
-    }
-    var vcapApplication,
-        vcapServices;
-
-    if (process.env.VCAP_APPLICATION) {
-        vcapApplication = JSON.parse(process.env.VCAP_APPLICATION);
+    catch (ex) {
+      // package.json could not be loaded from the cwd
     }
 
-    if ((vcapApplication) && (pkg)) {
-        var event = {
-            date_sent: new Date().toJSON()
-        };
-        if (pkg.version) {
-            event.code_version = pkg.version;
+    if (pkg) {
+      var event = {
+        date_sent: new Date().toJSON()
+      };
+      if (pkg.version) {
+        event.code_version = pkg.version;
+      }
+      if (pkg.repository) {
+        if (typeof pkg.repository === 'string') {
+          event.repository_url = pkg.repository;
+        } else if (pkg.repository.url) {
+          event.repository_url = pkg.repository.url;
         }
-        if (pkg.repository && pkg.repository.url) {
-            event.repository_url = pkg.repository.url;
-        }
-        if (vcapApplication.application_name) {
-            event.application_name = vcapApplication.application_name;
-        }
-        if (vcapApplication.space_id) {
-            event.space_id = vcapApplication.space_id;
-        }
-        if (vcapApplication.application_version) {
-            event.application_version = vcapApplication.application_version;
-        }
-        if (vcapApplication.application_uris) {
-            event.application_uris = vcapApplication.application_uris;
-        }
-        if (process.env.VCAP_SERVICES) {
-            // refer to http://docs.cloudfoundry.org/devguide/deploy-apps/environment-variable.html#VCAP-SERVICES
-            vcapServices =  JSON.parse(process.env.VCAP_SERVICES);
-            if(Object.keys(vcapServices).length > 0) {
-                event.bound_vcap_services = {};
-                // for each bound service count the number of instances and identify used plans
-                Object.keys(vcapServices).forEach(function(service_label) {
-                    event.bound_vcap_services[service_label] = {
-                                                                 'count': vcapServices[service_label].length,   // number of service_label instances
-                                                                 'plans': []                                    // (optional) plan information for service_label
-                                                               };
-                    vcapServices[service_label].forEach(function (serviceInstance) {
-                        if(serviceInstance.hasOwnProperty('plan')) {
-                            event.bound_vcap_services[service_label].plans.push(serviceInstance.plan);
-                        }
-                    });
+      }
+      if (pkg.name) {
+        event.application_name = pkg.name;
+      }
+      if (pkg.version) {
+        event.application_version = pkg.version;
+      }
+      event.platform = process.platform;
+      event.runtime = 'electron';
 
-                    // Keep plans property only if at least one plan is associated with this service
-                    if(event.bound_vcap_services[service_label].plans.length === 0) {
-                        delete event.bound_vcap_services[service_label].plans;
-                    }
-                });
-            }
-        }
-        event.runtime = 'nodejs';
-
-        var url = 'https://deployment-tracker.mybluemix.net/api/v1/track';
-        restler.postJson(url, event).on('complete', function (data) {
-            console.log('Uploaded stats', data);
-        });
+      HTTPPost(event, function() {
+        console.log('Uploaded stats');
+      });
     }
-}
+};
 
 module.exports.track = track;
